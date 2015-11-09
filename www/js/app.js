486523f2d237fca1d1d6e976c705bc387252aef9
Vivek Bharatia <vivek.bharatia@emc.com>
2015-11-09 10:25:27 +0530

    Added input validation

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('businesstracker', ['ionic','businesstracker.controllers','businesstracker.services'])
+angular.module('businesstracker', ['ionic','businesstracker.controllers','businesstracker.services','businesstracker.validation'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
